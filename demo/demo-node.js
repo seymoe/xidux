1147f7edd1212828630c6a3e9a7bf0e0d6c86a59
molang <ximolang@yeah.net>
2018-10-16 12:32:43 +0800

    change sth

--- demo/demo-node.js
+++ demo/demo-node.js
@@ -2,27 +2,52 @@ var xidux = require('../dist/index.js')
 
 // test
 const state = {
-  a: 1
-}
-
-function stateChanger (action, state) {
-  switch (action.type) {
-    case 'CHANGE_A':
-      state.a = action.data.a
-      break
-    default:
-      break
+  title: 'xidux text',
+  userInfo: {
+    nickname: 'ximolang',
+    age: 24
   }
 }
 
-const store = xidux.createStore(state, stateChanger)
+function reducer (action, state) {
+  switch (action.type) {
+    case 'CHANGE_TITLE':
+      return {
+        ...state,
+        title: action.title
+      }
+    case 'CHANGE_AGE':
+      return {
+        ...state,
+        userInfo: {
+          ...state.userInfo,
+          age: action.age
+        }
+      }
+    default:
+      return state
+  }
+}
+
+const store = xidux.createStore(reducer, state)
+
+store.subscribe(() => {
+  console.log('变更数据 ------')
+})
+
 console.log('状态变更前 ', store.getState())
 
 store.dispatch({
-  type: 'CHANGE_A',
-  data: {
-    a: 2
-  }
+  type: 'CHANGE_TITLE',
+  title: 'Hi, xidux'
 })
 
 console.log('状态变更后 ', store.getState())
+
+store.dispatch({
+  type: 'CHANGE_AGE',
+  age: 23
+})
+
+console.log('状态变更后, age为 ', store.getState().userInfo.age)
+
--- src/createStore.js
+++ src/createStore.js
@@ -1,8 +1,57 @@
-export default function createStore(state, stateChanger) {
-  const getState = () => state
-  const dispatch = (action) => stateChanger(action, state)
+import isPlainObject from './utils/isPlainObject'
+
+export default function createStore(reducer, preloadState) {
+  // 订阅者列表
+  const listeners = []
+  let currentState = preloadState
+  // 获取当前的state
+  const getState = () => currentState
+
+  // 添加订阅函数
+  const subscribe = (listener) => {
+    if (typeof listener !== 'function') {
+      throw new Error('listener should be a function.')
+    }
+    let isSubscribe = true
+    listeners.push(listener)
+
+    return function unsubscribe() {
+      if (!isSubscribe) return false
+
+      isSubscribe = false
+      let index = listeners.indexOf(listener)
+      listeners.splice(index, 1)
+    }
+  }
+
+  // 触发state改变
+  const dispatch = (action) => {
+    if (!isPlainObject(action)) {
+      throw new Error('action should be a plain object.')
+    }
+    if (action.type === undefined) {
+      throw new Error('`action.type` is required')
+    }
+    currentState = reducer(action, currentState)
+    console.log('dispath -> ', currentState)
+    // 更新state，便执行订阅的函数
+    if (listeners.length > 0) {
+      listeners.forEach(listener => {
+        listener()
+      })
+    }
+    
+    return action
+  }
+
+  // 初始化store
+  dispatch({
+    type: '@@xidux/INIT_STORE'
+  })
+
   return {
     getState,
-    dispatch
+    dispatch,
+    subscribe
   }
 }
